6ae40d7766ccdd760d9ac46d14ff25392c0fd158
vitalir2 <vitalyi100@mail.ru>
2022-12-04 09:10:49 +0300

    Complete 4th day, part 2

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -7,17 +7,26 @@ object Day04 : Challenge {
     override val day: Int = 4
 
     override fun part1(input: List<String>): Int {
+        return countNumberOfElvesPairs(input) { firstInterval, secondInterval ->
+            firstInterval.containsAll(secondInterval) || secondInterval.containsAll(firstInterval)
+        }
+    }
+
+    override fun part2(input: List<String>): Int {
+        return countNumberOfElvesPairs(input) { firstInterval, secondInterval ->
+            (firstInterval intersect secondInterval).isNotEmpty()
+        }
+    }
+
+    private fun countNumberOfElvesPairs(
+        input: List<String>,
+        predicate: (firstInterval: Set<Int>, secondInterval: Set<Int>) -> Boolean,
+    ): Int {
         return input
             .asSequence()
             .map { pairOfElves -> pairOfElves.split(",") }
             .map { elves -> elves.map { it.toIntRange("-").toSet() } }
-            .filter { (firstElfInterval, secondElfInterval) ->
-                firstElfInterval.containsAll(secondElfInterval) || secondElfInterval.containsAll(firstElfInterval)
-            }
+            .filter { (firstInterval, secondInterval) -> predicate(firstInterval, secondInterval) }
             .count()
-    }
-
-    override fun part2(input: List<String>): Int {
-        TODO("Not yet implemented")
     }
 }
